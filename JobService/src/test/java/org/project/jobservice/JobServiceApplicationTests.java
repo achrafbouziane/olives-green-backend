c63fd5ace52a12ebcdf1ea397bf4633fd85f0cd9
achraf <bouziane-achraf@emsi-edu.ma>
2025-11-13 14:36:04 +0100

    setup in architecture

---
+++ JobService/src/test/java/org/project/jobservice/JobServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.jobservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ ScheduleService/src/test/java/org/project/scheduleservice/ScheduleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.scheduleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
